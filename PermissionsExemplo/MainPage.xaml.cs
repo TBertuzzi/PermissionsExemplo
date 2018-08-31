21cb1e8ed6fa96de6c2e698d4e599be69a2d6243
Thiago Bertuzzi <thiago.bertuzzi@gmail.com>
2018-08-31 11:01:19 -0300

    :tada: Subindo para o Github o Exemplo

---
+++ PermissionsExemplo/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Plugin.Permissions;
+using Plugin.Permissions.Abstractions;
+using Xamarin.Forms;
+
+namespace PermissionsExemplo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void GPS_Clicked(object sender, System.EventArgs e)
+        {
+            var status = await CrossPermissions.Current.CheckPermissionStatusAsync(Permission.Location);
+            if (status != PermissionStatus.Granted)
+            {
+                var results = await CrossPermissions.Current.RequestPermissionsAsync(Permission.Location);
+                
+                if (results.ContainsKey(Permission.Location))
+                    status = results[Permission.Location];
+            }
+        }
+
+        async void Foto_Clicked(object sender, System.EventArgs e)
+        {
+            var status = await CrossPermissions.Current.CheckPermissionStatusAsync(Permission.Photos);
+            if (status != PermissionStatus.Granted)
+            {
+                var results = await CrossPermissions.Current.RequestPermissionsAsync(Permission.Photos);
+
+                if (results.ContainsKey(Permission.Location))
+                    status = results[Permission.Location];
+            }
+        }
+    }
+}
